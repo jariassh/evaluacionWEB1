609e7464c2a2e5b94f5e5a29456d7450d9682b14
Jonathan Arias <informesarias@gmail.com>
2020-09-05 17:16:51 -0500

    Iniciando
    Se crean cabezote general, con botonera que aparecerá en todas las paginas, se aplican estilos css y bootstrap

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -1,1 +1,70 @@
-HOLA SOY LA PLANTILLA PARA WEB 1
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Evaluación Web 1</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+	<!-- CSS Styles -->
+	<link rel="stylesheet" href="vistas/css/estilos.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+	<!-- Latest FontAwesom version -->
+	<script src="https://kit.fontawesome.com/29f215aa7a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+	<div class="container-fluid" id="logo">
+		<a href="#">
+			<img class="mx-auto d-block py-3" src="vistas/img/logo.png">
+		</a>
+	</div>
+
+	<div class="container-fluid bg-light">
+		<div class="container">
+			<ul class="nav nav-justified py-2 nav-pills">
+				<li class="nav-item">
+					<a href="#" class="nav-link font-weight-bold active">Inicio</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link font-weight-bold">Matemático</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link font-weight-bold">Gimnasio Bodytech</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link font-weight-bold">Spring Step</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link font-weight-bold">Postobón</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link font-weight-bold">
+						<i class="fas fa-bars"></i>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+
+<footer class="text-center py-4">
+	<h6 class="font-weight-bold">Copyright &copy 2020-2</h6>
+</footer>
+
+
+
+		
+</body>
+</html>
